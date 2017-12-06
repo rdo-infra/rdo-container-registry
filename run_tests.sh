21e9761404b4f549d8ce4c4bb81d1e907aa16311
Gonéri Le Bouder <goneri@redhat.com>
2017-12-06 12:06:29 -0500

    WIP: Configure the projects
    - some of the post deployment steps from registry.rdoproject.org
    - registry.distributed-ci.io
    - WIP because the secret generation is not finished yet
    
    Change-Id: If32684ad4b6ed6e3614970d41d22c60d48da9623

--- run_tests.sh
+++ run_tests.sh
@@ -11,7 +11,7 @@ function cleanup() {
 }
 
 # This runs on localhost but uses registry.rdoproject.org resources
-for host in registry.rdoproject.org console.registry.rdoproject.org trunk.registry.rdoproject.org
+for host in registry.rdoproject.org console.registry.rdoproject.org trunk.registry.rdoproject.org registry.distributed-ci.io
 do
     if ! grep -q "127.0.0.1 ${host}" /etc/hosts; then
         echo "127.0.0.1 ${host}" | sudo tee -a /etc/hosts
@@ -28,14 +28,19 @@ fi
 ssh-keyscan -H registry.rdoproject.org >>~/.ssh/known_hosts
 
 cleanup
-tox -e ansible-playbook -- -b -i hosts host-preparation.yml -e "ansible_ssh_user=${USER}"
+tox -e ansible-playbook -- -b -i hosts host-preparation.yml -e "ansible_ssh_user=${USER}" -M openshift-ansible/roles/lib_openshift/library
 cleanup
 # https://github.com/openshift/openshift-ansible/issues/5812
 # Glean configures "NM_CONTROLLED=no" in the ifcfg-eth0 file
 tox -e ansible-playbook -- -b -i hosts openshift-ansible/playbooks/byo/openshift-node/network_manager.yml -e "ansible_ssh_user=${USER}"
 cleanup
 tox -e ansible-playbook -- -b -i hosts openshift-ansible/playbooks/byo/config.yml -e "ansible_ssh_user=${USER}"
+cleanup
+tox -e ansible-playbook -- -b -i hosts projects-creation.yml -e "ansible_ssh_user=${USER}" -M openshift-ansible/roles/lib_openshift/library
 
 sudo oc get pods
 sudo oc get routes
 sudo oc get svc
+sudo oc get projects
+sudo oc policy who-can resource cluster-admin
+sudo oc get serviceaccounts --all-namespaces=true
