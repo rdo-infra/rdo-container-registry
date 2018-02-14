82635e65441c3d2fe3caa381bc27918b9523409c
David Moreau Simard <dmsimard@redhat.com>
2018-02-14 08:49:50 -0500

    WIP: Add pruning documentation, script and playbook/roles
    Change-Id: Ib4c65771e4e9e464ec8bfa1151cdcb8049864489

---
+++ roles/image-pruning/files/openshift_tag_pruner.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+#   Copyright Red Hat, Inc. All Rights Reserved.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import argparse
+from datetime import datetime
+import json
+import logging
+import logging.config
+import os
+import shlex
+import subprocess
+import sys
+import time
+import yaml
+
+
+def setup_logging(level):
+    log_config = """
+    ---
+    version: 1
+    formatters:
+        console:
+            format: '%(asctime)s %(levelname)s %(name)s: %(message)s'
+    handlers:
+        console:
+            class: logging.StreamHandler
+            formatter: console
+            level: {level}
+            stream: ext://sys.stdout
+    loggers:
+        {name}:
+            handlers:
+                - console
+            level: {level}
+            propagate: 0
+    root:
+      handlers:
+        - console
+      level: {level}
+    """.format(name=os.path.basename(__file__), level=level).lstrip()
+    logging.config.dictConfig(yaml.safe_load(log_config))
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", help="Enable debug logging",
+                        action="store_true")
+    parser.add_argument("--confirm", help="Confirm the deletion of the tags"
+                                          " (Dry-run by default)",
+                        action="store_true")
+    parser.add_argument("--days", help="Delete tags older than N days",
+                        default=7)
+    parser.add_argument("namespace", help="Namespace to prune old tags in")
+    args = parser.parse_args()
+    return args
+
+
+def run_command(command, log, fatal=False, confirm=False):
+    if not confirm:
+        log.info("Not running (dry-run): %s" % command)
+        return True, ""
+
+    try:
+        log.debug("Running: %s" % command)
+        output = subprocess.check_output(shlex.split(command),
+                                         stderr=subprocess.PIPE)
+        return True, output
+    except subprocess.CalledProcessError as e:
+        if fatal:
+            log.critical("Command failed: %s" % e.output)
+            raise e
+        else:
+            return False, e.output
+
+
+def main():
+    args = get_args()
+    level = "DEBUG" if args.debug else "INFO"
+    setup_logging(level)
+    log = logging.getLogger(os.path.basename(__file__))
+    log.debug("Arguments: %s" % json.dumps(args.__dict__))
+    if not args.confirm:
+        log.warn("{0} RUNNING IN DRY MODE {0}".format("*" * 10))
+    else:
+        log.warn("{0} CONFIRMED DELETION OF TAGS {0}".format("*" * 10))
+        time.sleep(5)
+
+    # Seconds in a day * args.days
+    MAX_AGE = 86400 * args.days
+    NOW = datetime.utcnow()
+
+    # These tags should never be deleted
+    # TODO(dmsimard): Make this an argument of the script
+    WHITELIST = [
+        'current-tripleo',
+        'current-tripleo-rdo',
+        'current-tripleo-rdo-internal',
+        'tripleo-ci-testing',
+        'latest',
+    ]
+    log.info("Deleting tags from %s older than %s days" % (args.namespace,
+                                                           args.days))
+
+    try:
+        cmd = "oc get -n %s istag -o json" % args.namespace
+        # This should always run regardless of dry-run or not
+        success, istags = run_command(cmd, log, confirm=True, fatal=True)
+        istags = json.loads(istags)
+    except (ValueError, TypeError) as e:
+        # Re-raise with a slightly friendlier message
+        log.critical("Unable to load tags from JSON: %s" % str(e))
+        raise e
+    log.info("%s total image tags found." % len(istags['items']))
+
+    log.info("%s tag names whitelisted: %s" % (
+        len(WHITELIST), ", ".join(WHITELIST))
+    )
+    # We need to iterate a first time across the list of all available tags
+    # in order to retrieve image tags which might match the same image.
+    for istag in istags['items']:
+        name, tag = istag['metadata']['name'].split(':')
+
+        for tag in image['status']['tags']:
+            if tag['tag'] in WHITELIST:
+                # 3b1e1c01-c7e6-11e7-bb2f-fa163e7f8b33
+                image['dockerImageMetadata']['']
+                print(image['metadata']['name'])
+                break
+
+    whitelisted = []
+    old_tags = []
+    for istag in istags['items']:
+        # Get the name, tag and creation timestamp of the image stream tag
+        name, tag = istag['metadata']['name'].split(':')
+        timestamp = istag['metadata']['creationTimestamp']
+        timestamp = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+
+        # If the image is older than MAX_AGE, add it to the list to delete
+        delta = NOW - timestamp
+        if tag in WHITELIST:
+            log.debug("%s:%s is whitelisted" % (name, tag))
+            whitelisted.append("%s:%s" % (name, tag))
+        else:
+            # Verify if this is a tag matching a whitelisted tag
+
+            if delta.total_seconds() > MAX_AGE:
+                log.debug("%s:%s to be deleted: %s" % (name, tag, delta))
+                old_tags.append("%s:%s" % (name, tag))
+            else:
+                log.debug("%s:%s won't be deleted: %s" % (name, tag, delta))
+
+    if len(whitelisted):
+        log.info("%s tags protected by whitelist." % len(whitelisted))
+
+    if len(old_tags):
+        log.info("%s tags will be deleted." % len(old_tags))
+    else:
+        log.info("Did not find any tags to delete.")
+        sys.exit(0)
+
+    failed = []
+    for old_tag in old_tags:
+        # Delete tags
+        cmd = "oc delete -n %s istag %s" % (args.namespace, old_tag)
+        success, output = run_command(cmd, log, confirm=args.confirm)
+        if not success:
+            failed.append(old_tag)
+
+    if len(failed):
+        log.error("%s tags were not deleted successfully." % len(failed))
+        for tag in failed:
+            log.error(tag)
+
+    log.info("Finished.")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
